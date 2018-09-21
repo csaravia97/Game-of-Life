1c5d904de9ec1a5cd5bff2e775cf45ecc07f5265
Carlos <c.rsaravia@yahoo.com>
2018-09-20 23:47:08 -0400

    Uploading game of life files

---
+++ TestProgram.cpp
@@ -0,0 +1,171 @@
+/*
+ * CSc103 Project 3: Game of Life
+ * See readme.html for details.
+ * Please list all references you made use of in order to complete the
+ * assignment: your classmates, websites, etc.  Aside from the lecture notes
+ * and the book, please list everything.  And remember- citing a source does
+ * NOT mean it is okay to COPY THAT SOURCE.  What you submit here **MUST BE
+ * YOUR OWN WORK**.
+ * References:
+ *
+ *
+ * Finally, please indicate approximately how many hours you spent on this:
+ * #hours:
+ */
+
+#include<fstream>
+#include<iostream>
+#include <cstdio>
+#include <stdlib.h> // for exit();
+#include <getopt.h> // to parse long arguments.
+#include <unistd.h> // sleep
+#include <vector>
+using std::vector;
+#include <string>
+using namespace std;
+
+static const char* usage =
+"Usage: %s [OPTIONS]...\n"
+"Text-based version of Conway's game of life.\n\n"
+"   --seed,-s     FILE     read start state from FILE.\n"
+"   --world,-w    FILE     store current world in FILE.\n"
+"   --fast-fw,-f  NUM      evolve system for NUM generations and quit.\n"
+"   --help,-h              show this message and exit.\n";
+
+size_t max_gen = 0; /* if > 0, fast forward to this generation. */
+string wfilename =  "/tmp/gol-world-current"; /* write state here */
+FILE* fworld = 0; /* handle to file wfilename. */
+string initfilename = "/tmp/gol-world-current"; /* read initial state from here. */
+
+size_t nbrCount(size_t i, size_t j, const vector<vector<bool> >&g);
+void update();
+int initFromFile(const string& fname);
+void mainLoop();
+void dumpState(FILE* f);
+void vectorofvector(const vector<vector<char> >&v);
+
+char text[3] = ".O";
+
+int main(int argc, char *argv[]) {
+	// define long options
+	static struct option long_opts[] = {
+		{"seed",    required_argument, 0, 's'},
+		{"world",   required_argument, 0, 'w'},
+		{"fast-fw", required_argument, 0, 'f'},
+		{"help",    no_argument,       0, 'h'},
+		{0,0,0,0}
+	};
+	// process options:
+	char c;
+
+	int opt_index = 0;
+	while ((c = getopt_long(argc, argv, "hs:w:f:", long_opts, &opt_index)) != -1) {
+		switch (c) {
+			case 'h':
+				printf(usage,argv[0]);
+				return 0;
+			case 's':
+				initfilename = optarg;
+				break;
+			case 'w':
+				wfilename = optarg;
+				break;
+			case 'f':
+				max_gen = atoi(optarg);
+				break;
+			case '?':
+				printf(usage,argv[0]);
+				return 1;
+		}
+	}
+
+
+mainLoop();
+	return 0;
+}
+
+void mainLoop(){
+	/* update, write, sleep */
+}
+#if 1
+
+void dumpState(FILE* f){
+	char c;
+FILE * f=fopen("/home/csc103/project-game/Game/res/glider-40x20","rb");
+	if(!f){
+		printf("error \n");
+	exit(1);}
+	while(fread(&c,1,1,f)> 0){
+		printf( "%c\n",c);}
+    fclose(f);
+return;
+	}
+#endif
+#if 0
+void vectorofvector(const vector<vector<char> >&v,){
+
+dumpState(FILE* f);
+	while(char){
+		v.push_back(c);
+	}
+}
+
+#endif
+#if 1
+}
+
+int check(int i; int j; vector<vector<bool> >& g){
+  size_t count=0;
+	for(i = 0; i < g.size(); i++){
+		for(j = 0; j < g[0].size(); j++){
+
+	if (g[i][(j-1+g[0].size())%g[0].size()])  {count++;}
+
+	if (g[i][(j+1+g[0].size())%g[0].size()])  {count++;}
+
+	if (g[(i-1+g.size())%g.size()][j])  {count++;}
+
+	if (g[(i+1+g.size())%g.size()][(j+1+g[0].size())%g[0].size())]){count++;}
+
+	if (g[(i-1+g.size()%g.size())][(j-1+g[0].size())%g[0].size()])   {count++;}
+
+	if (g[(i-1+g.size())%g.size())][(j+1+g[0].size())%g[0].size()])  {count++;}
+
+	if (g[(i+1+g.size())%g.size()][(j-1+g[0].size())%g[0].size()])   {count++;}
+
+	if (g[(i+1+g.size())%g.size()][(j+1+g[0].size())%g[0].size())    {count++;}
+}}
+	return count;
+}
+
+void makegrid(const vector<vector<bool> >&grid)
+{
+  for(int x=0;  x< grid.size(); x++)
+  {
+    for(int y= 0; y< grid[x].size(); y++)
+    {
+      cout <<g[x][y];
+    }
+		cout<<endl;
+	}
+}//global values//
+
+
+
+bool rules(int count){
+	if(count < 2){
+		return false;
+	}else if(count == 3){
+		return true;
+	}else if (count > 3){
+		return false;
+	}
+}
+FILE* f;
+f=fopen
+("/home/csc103/project-game/Game/res/glider-40x20","rb");
+if(f) {
+	while((c=getc(f)) !=EOF)
+    putchar(c);
+    fclose(f);
+#endif
---
+++ rulestest.cpp
@@ -0,0 +1,185 @@
+#include<fstream>
+#include<iostream>
+#include <cstdio>
+#include <stdlib.h> // for exit();
+#include <getopt.h> // to parse long arguments.
+#include <unistd.h> // sleep
+#include <vector>
+using std::vector;
+#include <string>
+using namespace std;
+
+static const char* usage =
+"Usage: %s [OPTIONS]...\n"
+"Text-based version of Conway's game of life.\n\n"
+"   --seed,-s     FILE     read start state from FILE.\n"
+"   --world,-w    FILE     store current world in FILE.\n"
+"   --fast-fw,-f  NUM      evolve system for NUM generations and quit.\n"
+"   --help,-h              show this message and exit.\n";
+
+size_t max_gen = 0; /* if > 0, fast forward to this generation. */
+string wfilename =  "/tmp/gol-world-current"; /* write state here */
+FILE* fworld = 0; /* handle to file wfilename. */
+string initfilename = "/tmp/gol-world-current"; /* read initial state from here. */
+
+vector<vector<bool> > old;
+
+	void printVecV(vector<vector<bool> > G);
+	int check(int i, int j, vector<vector<bool> > g);
+	void initFromFile(const string& fname);
+	void writeToFile(const string & fname);
+	void update();
+	void mainloop();
+
+	char text[3] = ".O";
+
+int main(int argc, char *argv[]) {
+	// define long options
+	static struct option long_opts[] = {
+		{"seed",    required_argument, 0, 's'},
+		{"world",   required_argument, 0, 'w'},
+		{"fast-fw", required_argument, 0, 'f'},
+		{"help",    no_argument,       0, 'h'},
+		{0,0,0,0}
+	};
+	// process options:
+	char c;
+	int opt_index = 0;
+	while ((c = getopt_long(argc, argv, "hs:w:f:", long_opts, &opt_index)) != -1) {
+		switch (c) {
+			case 'h':
+				printf(usage,argv[0]);
+				return 0;
+			case 's':
+				initfilename = optarg;
+				break;
+			case 'w':
+				wfilename = optarg;
+				break;
+			case 'f':
+				max_gen = atoi(optarg);
+				break;
+			case '?':
+				printf(usage,argv[0]);
+				return 1;
+		}
+	}
+
+
+	mainloop();
+	return 0;
+
+
+
+
+
+}
+
+
+
+
+	int check(int i, int j, vector<vector<bool> > g){
+  int count=0;
+
+		size_t r = g.size();
+		size_t c = g[0].size();
+
+	if (g[(i+r)%r][(j+1+c)%c])  				{count++;}		        //right
+
+	if (g[(i+r)%r][(j-1+c)%c])  				{count++;}		  	   //left
+
+	if (g[(i-1+r)%r][(j+c)%c])  				{count++;}			     //up
+
+	if (g[(i+1+r)%r][(j+c)%c])					{count++;}		  	  //down
+
+	if (g[(i-1+r)%r][(j-1+c)%c])  			{count++;}		     //up left
+
+	if (g[(i-1+r)%r][(j+1+c)%c])  			{count++;}		    //up right
+
+	if (g[(i+1+r)%r][(j-1+c)%c])   			{count++;}		   //down left
+
+	if (g[(i+1+r)%r][(j+1+c)%c])    		{count++;}	    //down right
+
+	return count;
+}
+
+
+	void printVecV(vector<vector<bool> > G){
+		int count=0;
+	for (int i = 0; i < G.size() ; i++) {
+	for(int j = 0;j < G[i].size() ;j ++) {
+		if(G[i][j]){
+				cout<<'O';
+			}else{
+				cout<<'.';
+			}
+	}
+	cout << endl;
+}
+}
+
+void initFromFile(const string& fname){
+
+	string line;
+	ifstream myfile(fname);
+	if(myfile.is_open()){
+
+		while(getline (myfile,line)){
+			vector<bool> b;
+
+			for(int i = 0; i < line.length(); i++){
+						if(line[i] == '.') {
+							b.push_back(false);
+						}else {
+										b.push_back(true);
+						}
+					}
+					old.push_back(b);}
+				myfile.close();
+
+				}else cout << "Unable to open file" << endl;
+
+	}
+
+	void update(){
+
+		vector<vector<bool> > updated;
+		for(int i = 0; i < old.size(); i++){
+		vector<bool> newrow;
+			for(int j = 0; j < old[0].size(); j++){
+				int count = check(i,j,old);
+				if(count > 3 || count < 2) newrow.push_back(false);
+				if(count == 3) newrow.push_back(true);
+				else newrow.push_back(old[i][j]);
+			}
+			updated.push_back(newrow);
+		}
+		old = updated;
+	}
+
+	void writeToFile(const string & fname){
+		ofstream myfile(fname);
+		if(myfile.is_open()){
+			for(size_t i = 0; i < old.size();i++){
+			for(size_t j = 0; j < old[0].size();j++){
+				if(old[i][j])myfile << 'O';
+				else myfile << '.';
+				}
+				myfile << endl;
+				}
+				myfile.close();
+			}
+			else cout << "Can't write file" << endl;
+			}
+
+	void mainloop(){
+		string fname;
+		size_t i = old.size();
+		size_t j = old[0].size();
+		initFromFile(fname);
+		check(i,j,old);
+		update();
+		writeToFile(fname);
+		printVecV(old);
+		sleep(1);
+	}
